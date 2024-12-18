e51631efdb0dc11802f1035478db2a8a2c103329
Amirul Islam Islam <amirul@ba-systems.com>
2024-12-18 18:05:02 +0600

    first commit

---
+++ src/songs/songs.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('songs')
+export class SongsController {
+  @Post()
+  create() {
+    return 'create a new song';
+  }
+  @Get()
+  findAll() {
+    return 'find all songs';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'find one song';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song based on id';
+  }
+}
